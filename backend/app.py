87f6faba686d79bbb81df71f508d11d304bda4f5
Sharathkumar H V <1js20cs147@gmail.com>
2025-12-27 18:58:24 +0530

    Added requirements and dynamic port for hosting

--- backend/app.py
+++ backend/app.py
@@ -67,4 +67,6 @@ def download():
     return send_file(path, as_attachment=True)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
